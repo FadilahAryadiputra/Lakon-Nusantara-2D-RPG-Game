cca4d00c44b80e4f64aef938001b42bcfda8d66f
IScream00 <mahardikakw15@gmail.com>
2022-06-26 14:57:19 +0700

    New File
    Required new Scene, etc

--- Lakon Nusantara/Assets/Scripts/FollowCamera.cs
+++ Lakon Nusantara/Assets/Scripts/FollowCamera.cs
@@ -9,10 +9,26 @@ public class FollowCamera : MonoBehaviour {
 	public GameObject target;
 	public Vector3 offset;
 	Vector3 targetPos;
+
+    public BoxCollider2D boundBox;
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+    private Camera theCamera;
+    private float halfHeight;
+    private float halfWidth;
+
 	// Use this for initialization
 	void Start () {
 		targetPos = transform.position;
-	}
+
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
+ 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -29,6 +45,9 @@ public class FollowCamera : MonoBehaviour {
 
 			transform.position = Vector3.Lerp( transform.position, targetPos + offset, 1f);
 
+            float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+            float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+            transform.position = new Vector3(clampedX, clampedY, transform.position.z);
 		}
 	}
 }
